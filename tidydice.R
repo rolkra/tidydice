d72f02acfd33911560043a903f915233e56525d4
rolkra <31808268+rolkra@users.noreply.github.com>
2019-10-31 22:01:36 +0100

    Update tidydice.R

--- tidydice.R
+++ tidydice.R
@@ -8,6 +8,34 @@ library(explore)
 ##############################################################################
 ## roll_dice
 ##############################################################################
+#' Simulating rolling a dice. The result is returned as a tibble.
+#'
+#' @param data Data from a previous experiment
+#' @param times How many times a dice is rolled (or how many dice are rolled at the same time)
+#' @param rounds Number of rounds 
+#' @param success Which result is a success (default = 6)
+#' @param agg If TRUE, the result is aggregated (by experiment, rounds)
+#' @param sides Number of sides of the dice (default = 6)
+#' @param prob Vector of probability for each side of the dice
+#' @return Result of experiment as a tibble
+#' @examples
+#' # rolling a dice once
+#' roll_dice()
+#'
+#' # rolling a dice 10 times
+#' roll_dice(times = 10)
+#'
+#' # aggregate result
+#' roll_dice(times = 10, agg = TRUE)
+#'
+#' # rounds 
+#' roll_dice(times = 10, rounds = 3, agg = TRUE)
+#' 
+#' # experiments
+#' library(dplyr)
+#' roll_dice(times = 10, rounds = 3, agg = TRUE) %>% 
+#'   roll_dice(times = 12, rounds = 3, agg = TRUE) 
+#' @export
 
 roll_dice <- function(data, times = 1, rounds = 1, success = c(6), agg = FALSE, sides = 6, prob = NULL)  {
   
@@ -42,7 +70,7 @@ roll_dice <- function(data, times = 1, rounds = 1, success = c(6), agg = FALSE, 
   } # if agg
   
   if (missing(data))  {
-  
+    
     # result of roll_dice (first experiment)
     result <- result %>% 
       mutate(experiment = 1) %>% 
@@ -72,6 +100,34 @@ roll_dice <- function(data, times = 1, rounds = 1, success = c(6), agg = FALSE, 
 ##############################################################################
 ## flip_coin
 ##############################################################################
+#' Simulating flipping a coin. The result is returned as a tibble.
+#'
+#' @param data Data from a previous experiment
+#' @param times How many times coin is flipped (or how many coins are flipped at the same time)
+#' @param rounds Number of rounds 
+#' @param success Which result is a success (default = 2)
+#' @param agg If TRUE, the result is aggregated (by experiment, rounds)
+#' @param sides Number of sides of the coin (default = 2)
+#' @param prob Vector of probability for each side of the coin
+#' @return Result of experiment as a tibble
+#' @examples
+#' # flipping a coin
+#' flip_coin()
+#'
+#' # flipping a coin 10 times
+#' flip_coin(times = 10)
+#'
+#' # aggregate result
+#' flip_coin(times = 10, agg = TRUE)
+#'
+#' # rounds 
+#' flip_coin(times = 10, rounds = 3, agg = TRUE)
+#' 
+#' # experiments
+#' library(dplyr)
+#' flip_coin(times = 10, rounds = 3, agg = TRUE) %>% 
+#'   flip_coin(times = 12, rounds = 3, agg = TRUE) 
+#' @export
 
 flip_coin <- function(times = 1, rounds = 1, success = c(2), agg = FALSE, sides = 2, prob = NULL)  {
   
@@ -85,101 +141,68 @@ flip_coin <- function(times = 1, rounds = 1, success = c(2), agg = FALSE, sides 
 }
 
 ##############################################################################
-## plot_success
-##############################################################################
-
-plot_success <- function(data, title = "", label, label_size = 2)  {
-  
-  # if no label parameter, decide on
-  # number of bars to be plotted
-  bars <- length(unique(data$success)) * max(data$experiment)
-  if (missing(label)) {
-    if (bars <= 20)  {
-       label <- TRUE
-    } else {
-       label <- FALSE
-    }
-  }
-  
-  # use a factor for experiment so that fill works
-  data$experiment <- factor(data$experiment)
-  
-  data_experiment <- data %>% 
-    group_by(experiment) %>% 
-    summarise(experiment_n = n())
-  
-  data_success <- data %>% 
-    group_by(experiment, success) %>% 
-    summarise(success_sum = sum(success),
-              n = n())
-  
-  data_bar <- data_success %>% 
-    inner_join(data_experiment, by = "experiment") %>% 
-    mutate(pct = n / experiment_n * 100.0)
-  
-  #data_bar$success <- factor(data_bar$success)
-  
-  p <- ggplot(data_bar, aes(x = success)) +
-    geom_col(aes(y = pct, fill = experiment), position = "dodge") +
-    theme_minimal() +
-    labs(y = "percent")
-  
-  # plot labels?
-  if (label == TRUE)  {
-    p <- p + geom_text(aes(y = pct, 
-                           label = pct,
-                           group = experiment),
-                      #     label = formatC(success_pct, format = "f", digits = 1)),
-                       position = position_dodge(width = 1),
-                       vjust = 1,
-                       size = label_size)
-  } # if
-  
-  if (nchar(title) > 0)  {
-    
-    p <- p + ggtitle(title)
-    
-  }
-  
-  # plot result
-  p
-  
-} # plot_success
-
-
-##############################################################################
 ## examples
 ##############################################################################
 
 # reproducible random numbers
 set.seed(123)
 
-# rolling a dice 60 times
-d <- roll_dice(times = 60)
+# rolling a dice once
+roll_dice()
+
+# rolling a dice 10 times
+roll_dice(times = 10)
+
+# aggregate result
+roll_dice(times = 10, agg = TRUE)
+
+# rounds 
+roll_dice(times = 10, rounds = 3, agg = TRUE)
+
+# experiments 
+roll_dice(times = 10, rounds = 3, agg = TRUE) %>% 
+  roll_dice(times = 12, rounds = 3, agg = TRUE) 
+  
+# analysing
+d <- roll_dice(100)
 d %>% describe(result)
+d %>% explore(result)
 d %>% describe(success)
-d %>% plot_success()
+d %>% explore(success)
 
 # compare rolling a dice 10 times and then 20 times
 roll_dice(times = 10, agg = TRUE) %>% 
   roll_dice(times = 20, agg = TRUE) 
 
 # adding a title
-roll_dice(times = 10, agg = TRUE) %>% 
-  roll_dice(times = 20, agg = TRUE) %>% 
-  plot_success(title = "Dice 10x vs 20x")
+roll_dice(times = 10) %>% 
+  roll_dice(times = 20) %>% 
+  explore(success, target = experiment, title = "Dice 10x vs 20x")
 
 # comparing 3 experiments
-
 roll_dice(times = 60, rounds = 10000, agg = TRUE) %>% 
   roll_dice(times = 70, rounds = 10000, agg = TRUE) %>% 
   roll_dice(times = 100, rounds = 10000, agg = TRUE) %>% 
-  plot_success()
+  explore(success, target = experiment, title = "Dice 60x vs 70x vs 100x")
 
 # rolling a dice 60 times, repeat it 100 rounds
-roll_dice(times = 60, rounds = 100) %>% 
-  describe(result)
+roll_dice(times = 60, rounds = 100, agg = TRUE) %>% 
+  explore(success)
 
 # rolling an unfair dice 60 times, repeat it 100 rounds
-roll_dice(times = 60, rounds = 100, prob = c(1/12,1/6,1/6,1/6,1/6,1/6+1/12)) %>%
-  explore(result)
+roll_dice(times = 60, rounds = 100, prob = c(1/12,1/6,1/6,1/6,1/6,1/6+1/12), agg = TRUE) %>%
+  explore(success)
+
+# compare two experiments
+roll_dice(times = 60, rounds = 100, prob = c(1/6,1/6,1/6,1/6,1/6,1/6), agg = TRUE) %>%
+  roll_dice(times = 60, rounds = 100, prob = c(1/12,1/6,1/6,1/6,1/6,1/6+1/12), agg = TRUE) %>%
+  explore(success, target = experiment)
+
+# compare two experiments (bar chart)
+roll_dice(times = 60, rounds = 100, prob = c(1/6,1/6,1/6,1/6,1/6,1/6), agg = TRUE) %>%
+  roll_dice(times = 60, rounds = 100, prob = c(1/12,1/6,1/6,1/6,1/6,1/6+1/12), agg = TRUE) %>%
+  explore_bar(success, 
+              target = experiment, 
+              flip = FALSE,
+              title = "Rolling a dice 60 times, 100 rounds")
+
