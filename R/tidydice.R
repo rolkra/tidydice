e8c644321bde1c2a04f66fae9c731b5ba7bb678a
Krasser Roland <Roland.Krasser@A1.at>
2019-11-01 22:11:11 +0100

    tidydice package 0.0.1 (DRAFT)

--- R/tidydice.R
+++ R/tidydice.R
@@ -1,11 +1,4 @@
 ##############################################################################
-## setup
-##############################################################################
-
-library(tidyverse)
-library(explore)
-
-##############################################################################
 ## roll_dice
 ##############################################################################
 #' Simulating rolling a dice. The result is returned as a tibble.
@@ -18,6 +11,7 @@ library(explore)
 #' @param sides Number of sides of the dice (default = 6)
 #' @param prob Vector of probabilities for each side of the dice
 #' @return Result of experiment as a tibble
+#' @importFrom magrittr "%>%"
 #' @examples
 #' # rolling a dice once
 #' roll_dice()
@@ -44,28 +38,31 @@ roll_dice <- function(data, times = 1, rounds = 1, success = c(6), agg = FALSE, 
   stopifnot(rounds >= 1)
   stopifnot(sides >= 2)
   
+  # definition to pass CRAN test
+  experiment <- 0
+  
   if (agg)  {
     
     # roll dice and aggregate result
     rounds_seq <- 1:rounds
-    success <- map_int(rounds_seq, ~sum(sample(x = 1:sides, size = times, replace = TRUE, prob = prob) %in% success))
+    success <- purrr::map_int(rounds_seq, ~sum(sample(x = 1:sides, size = times, replace = TRUE, prob = prob) %in% success))
     
     # create result tibble
-    result <- tibble(round = rounds_seq,
-                     times = rep(times, rounds),
-                     success = success)
+    result <- tibble::tibble(round = rounds_seq,
+                             times = rep(times, rounds),
+                             success = success)
     
   } else { 
     
     # roll the dice: rounds x times
-    result <- tibble(round = rep(1:rounds, each = times),
-                     nr = rep(1:times, times = rounds),
-                     result = sample(x = 1:sides, size = rounds * times, replace = TRUE, prob = prob)
+    result <- tibble::tibble(round = rep(1:rounds, each = times),
+                             nr = rep(1:times, times = rounds),
+                             result = sample(x = 1:sides, size = rounds * times, replace = TRUE, prob = prob)
     )
     
     # determine success
     result <- result %>%
-      mutate(success = ifelse(result %in% success, TRUE, FALSE))
+      dplyr::mutate(success = ifelse(result %in% success, TRUE, FALSE))
     
   } # if agg
   
@@ -73,25 +70,24 @@ roll_dice <- function(data, times = 1, rounds = 1, success = c(6), agg = FALSE, 
     
     # result of roll_dice (first experiment)
     result <- result %>% 
-      mutate(experiment = 1) %>% 
-      select(experiment, everything())
+      dplyr::mutate(experiment = 1) %>% 
+      dplyr::select(experiment, dplyr::everything())
     result
     
   } else {
     
     # existing experiment variable?
+    max_experiment <- 1
     if ("experiment" %in% names(data)) {
       max_experiment <- max(data$experiment)
-    } else {
-      data$experiment <- 1
     }
     
     # new experiment (+1)
     result$experiment <- max_experiment + 1
     
     # bind result to data (pipe)
-    result <- bind_rows(data, result) %>% 
-      select(experiment, everything())
+    result <- dplyr::bind_rows(data, result) %>% 
+      dplyr::select(experiment, dplyr::everything())
     result
   }
   
@@ -129,7 +125,7 @@ roll_dice <- function(data, times = 1, rounds = 1, success = c(6), agg = FALSE, 
 #'   flip_coin(times = 12, rounds = 3, agg = TRUE) 
 #' @export
 
-flip_coin <- function(times = 1, rounds = 1, success = c(2), agg = FALSE, sides = 2, prob = NULL)  {
+flip_coin <- function(data, times = 1, rounds = 1, success = c(2), agg = FALSE, sides = 2, prob = NULL)  {
   
   # check if possible
   stopifnot(times >= 0)
@@ -137,72 +133,12 @@ flip_coin <- function(times = 1, rounds = 1, success = c(2), agg = FALSE, sides 
   stopifnot(sides == 2)
   
   # coin = dice with 2 sides
-  roll_dice(times, rounds, success, agg, sides, prob)
+  roll_dice(data = data,
+            times = times, 
+            rounds = rounds, 
+            success = success, 
+            agg = agg, 
+            sides = sides, 
+            prob = prob)
 }
-
-##############################################################################
-## examples
-##############################################################################
-
-# reproducible random numbers
-set.seed(123)
-
-# rolling a dice once
-roll_dice()
-
-# rolling a dice 10 times
-roll_dice(times = 10)
-
-# aggregate result
-roll_dice(times = 10, agg = TRUE)
-
-# rounds 
-roll_dice(times = 10, rounds = 3, agg = TRUE)
-
-# experiments 
-roll_dice(times = 10, rounds = 3, agg = TRUE) %>% 
-  roll_dice(times = 12, rounds = 3, agg = TRUE) 
-  
-# analysing
-d <- roll_dice(100)
-d %>% describe(result)
-d %>% explore(result)
-d %>% describe(success)
-d %>% explore(success)
-
-# compare rolling a dice 10 times and then 20 times
-roll_dice(times = 10, agg = TRUE) %>% 
-  roll_dice(times = 20, agg = TRUE) 
-
-# adding a title
-roll_dice(times = 10) %>% 
-  roll_dice(times = 20) %>% 
-  explore(success, target = experiment, title = "Dice 10x vs 20x")
-
-# comparing 3 experiments
-roll_dice(times = 60, rounds = 10000, agg = TRUE) %>% 
-  roll_dice(times = 70, rounds = 10000, agg = TRUE) %>% 
-  roll_dice(times = 100, rounds = 10000, agg = TRUE) %>% 
-  explore(success, target = experiment, title = "Dice 60x vs 70x vs 100x")
-
-# rolling a dice 60 times, repeat it 100 rounds
-roll_dice(times = 60, rounds = 100, agg = TRUE) %>% 
-  explore(success)
-
-# rolling an unfair dice 60 times, repeat it 100 rounds
-roll_dice(times = 60, rounds = 100, prob = c(1/12,1/6,1/6,1/6,1/6,1/6+1/12), agg = TRUE) %>%
-  explore(success)
-
-# compare two experiments
-roll_dice(times = 60, rounds = 100, prob = c(1/6,1/6,1/6,1/6,1/6,1/6), agg = TRUE) %>%
-  roll_dice(times = 60, rounds = 100, prob = c(1/12,1/6,1/6,1/6,1/6,1/6+1/12), agg = TRUE) %>%
-  explore(success, target = experiment)
-
-# compare two experiments (bar chart)
-roll_dice(times = 60, rounds = 100, prob = c(1/6,1/6,1/6,1/6,1/6,1/6), agg = TRUE) %>%
-  roll_dice(times = 60, rounds = 100, prob = c(1/12,1/6,1/6,1/6,1/6,1/6+1/12), agg = TRUE) %>%
-  explore_bar(success, 
-              target = experiment, 
-              flip = FALSE,
-              title = "Rolling a dice 60 times, 100 rounds")
 
